776d5099857b273858eeaac7d3f5e8c3823b96bd
vknanavati <vimin.nanavati@gmail.com>
2024-01-05 16:41:40 -0500

    used random module to randomize generation of cat fact.

--- cat_facts.py
+++ cat_facts.py
@@ -1,21 +1,20 @@
 import requests
 import json
+import random
 
 endpoint_url = 'https://cat-fact.herokuapp.com/facts'
 
 response = requests.get(endpoint_url)
 
-# print(type(response))
-# print(response.text)
-# print(response.headers.get("Content-Type"))
-# print(response.json())
+type(response)
+# response.text
+response.headers.get("Content-Type")
+response.json()
 
 data = response.content
 cat_dict = json.loads(data)
-# print(data_dict)
-# print(cat_dict[0]["text"])
-print(len(cat_dict))
 
-for index in range(len(cat_dict)):
-    print(cat_dict[index]["text"])
+rand_index = random.randint(0,len(cat_dict)-1)
+print(cat_dict[rand_index]["text"])
+
  
